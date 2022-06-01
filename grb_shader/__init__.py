55bbb64235264c177269cc3230fbcaeb92ae3d81
Elisa Schösser <eschoe@mpe.mpg.de>
2022-06-01 15:49:16 +0200

    Faster access to path of data files - not only one anymore

--- grb_shader/__init__.py
+++ grb_shader/__init__.py
@@ -2,7 +2,7 @@ from .simulation import play_god, RestoredSimulation
 from .catalog import LocalVolume
 from .grb_pop import GRBPop
 
-from .utils.package_data import get_ghirlanda_model
+from .utils.package_data import get_path_of_data_file, get_ghirlanda_model
 
 
 from ._version import get_versions
