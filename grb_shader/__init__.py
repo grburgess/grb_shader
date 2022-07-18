37b663a2ffc8a842367913a564d8f566785afcff
Elisa Schösser <eschoe@mpe.mpg.de>
2022-07-18 19:21:15 +0200

    Add matplotlib config file

--- grb_shader/__init__.py
+++ grb_shader/__init__.py
@@ -1,10 +1,15 @@
-from .simulation import GBM_GRB_God, RestoredSimulation
+from .simulation import GRBPop_God, Restored_GRBPops
 from .catalog import LocalVolume
 from .grb_pop import GRBPop
 
-from .utils.package_data import get_path_of_data_file, get_ghirlanda_model
+from .utils.package_data import get_path_of_data_file, get_ghirlanda_model,get_path_of_config_file
 
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+#configuration matplotlib
+from matplotlib import rc_file
+rc_file(get_path_of_config_file('matplotlibrc'))
