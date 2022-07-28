757670618aa8639b5a22122e36e23db4468b8bab
Elisa Schösser <eschoe@mpe.mpg.de>
2022-07-28 20:01:35 +0200

    add formatter and handler for console logs

--- grb_shader/utils/logging.py
+++ grb_shader/utils/logging.py
@@ -1,6 +1,19 @@
 import logging
 
-def setup_log(name):
+# create console handler and set level that is printed on console
+grb_shader_console_log_handler = logging.StreamHandler()
+grb_shader_console_log_handler.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    
+# add formatter to ch
+grb_shader_console_log_handler.setFormatter(formatter)
+
+def update_logging_level(level):
+    grb_shader_console_log_handler.setLevel(level)
+
+def setup_log(name='grb_shader'):
 
     # set up logger with name 
     log = logging.getLogger(name)
@@ -9,6 +22,9 @@ def setup_log(name):
     log.setLevel(logging.DEBUG)
 
     # donot duplicate messages
-    log.propagate = False
+    log.propagate = False  
+
+    # add ch to logger
+    log.addHandler(grb_shader_console_log_handler)
 
     return log
